ddfc7d698edc94fda472c4868f16cab14ba0f2a0
rafailvv <rafail.02@mail.ru>
2022-06-09 14:05:19 +0300

    New commit

--- aiogram introduction.py
+++ aiogram introduction.py
@@ -1,7 +1,8 @@
 import pprint
+from random import choice
 
 from aiogram import Bot, Dispatcher,executor
-from aiogram.types import Message
+from aiogram.types import Message, PollAnswer
 import asyncio
 
 from parserPhoto import ImageParsing
@@ -15,19 +16,26 @@ dp = Dispatcher(bot=bot, loop=loop)
 async def process_start_command(message: Message):
     await message.reply(text = f'Привет, {message.from_user.full_name}')
 
+@dp.poll_answer_handler()
+async def pool_answers(poolAnswer : PollAnswer):
+    print(poolAnswer.option_ids, poolAnswer.poll_id)
+    print(poolAnswer.get_current())
 
 @dp.message_handler()
 async def text_handler(message : Message):
-    # if 'отправь фото' in message.text.lower():
-    #     await message.answer_photo(photo=)
-    # else:
-    #     await message.answer(text=message.text)
-    # if str(message.text)[:5].lower().strip() == 'image':
-    dates = str(message.text).strip()
-    impa = ImageParsing(dates)
-    pprint.pprint(impa.parse_links())
-    photo = impa.download_image(impa.parse_links()[0], "image")
-    await message.answer_photo(photo=photo)
+    # Отправляет фотку по запросу
+    # dates = str(message.text).strip()
+    # impa = ImageParsing(dates)
+    # photo = impa.download_image(choice(impa.parse_links()), "image")
+    # await message.answer_photo(photo=photo)
+    # await bot.send_message(chat_id=, text=)
+
+    # await message.answer_voice(voice="AwACAgIAAxkBAAMlYqGxfHxMQLIgqmjQWy0ShJ7XkZgAAm4aAAKWeBFJGRgTgn7tc3gkBA")
+
+
+    # await message.answer_location(latitude=55.753816, longitude=48.743171)
+
+    await message.answer_poll(question="Как дела?", options=["Хорошо", "Пойдёт", "Ну такое"], is_anonymous=False)
 
 
 executor.start_polling(dp)
