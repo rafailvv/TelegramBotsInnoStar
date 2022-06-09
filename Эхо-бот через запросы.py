7e08916fb3d4131f774b2939f228012570bd3ebd
rafailvv <rafail.02@mail.ru>
2022-06-09 10:31:49 +0300

    New commit

--- Эхо-бот через запросы.py
+++ Эхо-бот через запросы.py
@@ -4,11 +4,11 @@ import asyncio
 
 updates = requests.get('https://api.telegram.org/bot5490426200:AAG4MUqJppCYScZNADDkcmesq52huUxYGXo/getUpdates').json()
 
-text_last_message = updates['result'][-1]['message']['text']
-chat_id = updates['result'][0]['message']['chat']['id']
-
-
-requests.get(f'https://api.telegram.org/bot5490426200:AAG4MUqJppCYScZNADDkcmesq52huUxYGXo/sendMessage?chat_id={chat_id}&text={text_last_message}')
+# text_last_message = updates['result'][-1]['message']['text']
+# chat_id = updates['result'][0]['message']['chat']['id']
+#
+#
+# requests.get(f'https://api.telegram.org/bot5490426200:AAG4MUqJppCYScZNADDkcmesq52huUxYGXo/sendMessage?chat_id={chat_id}&text={text_last_message}')
 
 len_spis = len(updates['result'])
 
